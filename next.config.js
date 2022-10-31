bf151538f05d8e7aaf7c711c1a81a173ff15044c
Lathryx <52394238+Lathryx@users.noreply.github.com>
2022-10-31 01:17:09 -0400

    `images.unoptimized = true`
    Hopefully this fixes my deployment issues (used last build log's error)!

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: true, 
+  images.unoptimized = true 
 }
 
 module.exports = nextConfig
